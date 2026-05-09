6d59694307b46e7992ccbe513185fc8e0b51d7af
vamshik9 <vamshikyatham9@gmail.com>
2026-05-09 16:28:29 +0000

    11001100

---
+++ 1100.sv
@@ -0,0 +1,26 @@
+class transaction;
+
+	rand bit data[];
+
+	constraint c1{
+		data.size() inside {[5:10]};
+		}
+	constraint c2{
+		foreach(data[i]){
+		   if(i%4 < 2) data[i] == 1;
+		   else        data[i] == 0;
+		     }
+		     }
+endclass
+
+module tb;
+ 
+transaction tx;
+
+initial begin
+	tx = new();
+	assert(tx.randomize());
+
+	$display(" 1100 patten data =%p",tx.data);
+end
+endmodule 
